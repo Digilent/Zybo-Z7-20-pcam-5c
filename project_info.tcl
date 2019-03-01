1878e144939a93b7e770f5f2f09f701dfbeca358
Elod Gyorgy <elod.gyorgy@digilent.ro>
2019-03-01 12:26:29 +0200

    MOD: switched to digilent_vivado_scripts versioning method NEW: updated to 2018.2

---
+++ project_info.tcl
@@ -0,0 +1,8 @@
+# This is an automatically generated file used by digilent_vivado_checkout.tcl to set project options
+proc set_digilent_project_properties {proj_name} {
+    set project_obj [get_projects $proj_name]
+	set_property "part" "xc7z020clg400-1" $project_obj
+	set_property "default_lib" "xil_defaultlib" $project_obj
+	set_property "simulator_language" "Mixed" $project_obj
+	set_property "target_language" "VHDL" $project_obj
+}
